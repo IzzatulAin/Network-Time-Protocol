d2250c82504cfae4c19ded7a1421331948b0bcfb
izzatulain <2019406096@isiswa.uitm.edu.my>
2021-06-06 16:45:31 +0800

    commit

--- time.c
+++ time.c
@@ -29,18 +29,13 @@ int main( int argc, char* argv[ ] )
 
   char* host_name = "us.pool.ntp.org"; // NTP server host-name.
 
-  // Structure that defines the 48 byte NTP packet protocol.
 
   typedef struct
   {
 
-    uint8_t li_vn_mode;      // Eight bits. li, vn, and mode.
-                             // li.   Two bits.   Leap indicator.
-                             // vn.   Three bits. Version number of the protocol.
-                             // mode. Three bits. Client will pick mode 3 for client.
-
-    uint8_t stratum;         // Eight bits. Stratum level of the local clock.
-    uint8_t poll;            // Eight bits. Maximum interval between successive messages.
+    uint8_t li_vn_mode;
+    uint8_t stratum;
+    uint8_t poll;
     uint8_t precision;       // Eight bits. Precision of the local clock.
 
     uint32_t rootDelay;      // 32 bits. Total round trip delay time.
@@ -61,18 +56,13 @@ int main( int argc, char* argv[ ] )
 
   } ntp_packet;              // Total: 384 bits or 48 bytes.
 
-  // Create and zero out the packet. All 48 bytes worth.
 
   ntp_packet packet = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
   memset( &packet, 0, sizeof( ntp_packet ) );
 
-  // Set the first byte's bits to 00,011,011 for li = 0, vn = 3, and mode = 3. The rest will be left set to zero.
 
-  *( ( char * ) &packet + 0 ) = 0x1b; // Represents 27 in base 10 or 00011011 in base 2.
-
-  // Create a UDP socket, convert the host-name to an IP address, set the port number,
-  // connect to the server, send the packet, and then read in the return packet.
+  *( ( char * ) &packet + 0 ) = 0x1b;
 
   struct sockaddr_in serv_addr; // Server address data structure.
   struct hostent *server;      // Server data structure.
@@ -87,54 +77,37 @@ int main( int argc, char* argv[ ] )
   if ( server == NULL )
     error( "ERROR, no such host" );
 
-  // Zero out the server address structure.
 
   bzero( ( char* ) &serv_addr, sizeof( serv_addr ) );
 
   serv_addr.sin_family = AF_INET;
 
-  // Copy the server's IP address to the server address structure.
+ 
 
   bcopy( ( char* )server->h_addr, ( char* ) &serv_addr.sin_addr.s_addr, server->h_length );
 
-  // Convert the port number integer to network big-endian style and save it to the server address structure.
 
   serv_addr.sin_port = htons( portno );
 
-  // Call up the server using its IP address and port number.
 
   if ( connect( sockfd, ( struct sockaddr * ) &serv_addr, sizeof( serv_addr) ) < 0 )
     error( "ERROR connecting" );
-
-  // Send it the NTP packet it wants. If n == -1, it failed.
 
   n = write( sockfd, ( char* ) &packet, sizeof( ntp_packet ) );
 
   if ( n < 0 )
     error( "ERROR writing to socket" );
 
-  // Wait and receive the packet back from the server. If n == -1, it failed.
 
   n = read( sockfd, ( char* ) &packet, sizeof( ntp_packet ) );
 
   if ( n < 0 )
     error( "ERROR reading from socket" );
 
-  // These two fields contain the time-stamp seconds as the packet left the NTP server.
-  // The number of seconds correspond to the seconds passed since 1900.
-  // ntohl() converts the bit/byte order from the network's to host's "endianness".
-
-  packet.txTm_s = ntohl( packet.txTm_s ); // Time-stamp seconds.
-  packet.txTm_f = ntohl( packet.txTm_f ); // Time-stamp fraction of a second.
-
-  // Extract the 32 bits that represent the time-stamp seconds (since NTP epoch) from when the packet left the server.
-  // Subtract 70 years worth of seconds from the seconds since 1900.
-  // This leaves the seconds since the UNIX epoch of 1970.
-  // (1900)------------------(1970)**************************************(Time Packet Left the Server)
+  packet.txTm_s = ntohl( packet.txTm_s );
+  packet.txTm_f = ntohl( packet.txTm_f ); 
 
   time_t txTm = ( time_t ) ( packet.txTm_s - NTP_TIMESTAMP_DELTA );
-
-  // Print the time we got from the server, accounting for local timezone and conversion from UTC time.
 
   printf( "Time: %s", ctime( ( const time_t* ) &txTm ) );
 
